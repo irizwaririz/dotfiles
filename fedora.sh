4a8103ee169c195cb5dbecc413d9befffff3d62c
John Carlo Roberto <10111643+Irizwaririz@users.noreply.github.com>
2022-09-28 19:13:59 +0800

    Add initial Fedora installation script

---
+++ fedora.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Update Fedora packages to the latest version
+sudo dnf upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo dnf install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# basic packages
+install curl
+install wget
+install make
+install gcc
+
+# development
+install git
+install stow
+install ripgrep
+install tmux
+
+# st dependencies
+install pkg-config
+install fontconfig-devel
+install libX11-devel
+install libXft-devel
+install dejavu-sans-mono-fonts.noarch 
+
+# vim dependencies
+install ncurses-devel
+
+# tmux dependencies
+install xclip
+
+# system management
+install htop
+install ncdu
+
+# Run all installation scripts in programs/
+for f in programs/*.sh; do bash "$f" -H; done
+
+# Symlink dotfiles
+# ./dotfiles.sh fedora
