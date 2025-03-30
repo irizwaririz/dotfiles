3fb3f9f58b294d489eb3910ece441b937405a0a9
John Carlo Roberto <10111643+Irizwaririz@users.noreply.github.com>
2025-03-31 03:43:03 +0800

    Add dmenu dependencies

--- fedora.sh
+++ fedora.sh
@@ -36,6 +36,9 @@ install dejavu-sans-mono-fonts.noarch
 # slock dependencies
 install libXrandr-devel
 
+# dmenu dependencies
+install libXinerama-devel
+
 # vim dependencies
 install ncurses-devel
 install libXt-devel
